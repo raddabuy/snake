09fd431c6f2b88a846e1867a6c5d55b39ae8fff9
ksenia <raddabuy@yandex.ru>
2018-09-17 21:42:02 +0300

    Создан класс Point

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-//using System.Threading.Tasks;
+using System.Threading.Tasks;
+using Snake;
+//using NameSpaceClassInIncludedFile;
 
 
 namespace snake
@@ -11,24 +13,27 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
+            /*int x1 = 1;
             int y1 = 3;
             char sym1 = '*';
 
-            Draw(x1,y1,sym1);
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
+            Draw(x1,y1,sym1);*/
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
 
-            Draw(x2, y2, sym2);
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
 
             Console.ReadLine();
         }
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
+       
     
     }
 }
